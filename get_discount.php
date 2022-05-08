6c93aee06f4e4c023fc0607137d2fcf2b59dfa80
hernan romero <60491656+hernanRom@users.noreply.github.com>
2022-05-08 08:26:09 -0300

    Add files via upload

---
+++ get_discount.php
@@ -0,0 +1,23 @@
+
+<?php
+	require_once 'dash/conexion.php';
+	$coupon_code = $_POST['coupon'];
+	$price = $_POST['price'];
+	
+	$query = mysqli_query($mysqli, "SELECT * FROM `coupon` WHERE `coupon_code` = '$coupon_code' && `status` = 'Valid'") or die(mysqli_error());
+	$count = mysqli_num_rows($query);
+	$fetch_cart = mysqli_fetch_array($query);
+	$array = array();
+	if($count > 0){
+		$discount = $fetch_cart['discount'] / 100;
+		$total = $discount * $price;
+		$array['discount'] = $fetch_cart['discount'];
+		$array['price'] = $price - $total;
+		
+		echo json_encode($array);
+		
+	}else{
+		echo "error";
+		
+	}
+?>
